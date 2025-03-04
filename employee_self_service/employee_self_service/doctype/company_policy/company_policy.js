7aa8588f2d6e8b88f48ed9cc76376c4d17cacb1d
nesscale-com <info@nesscale.com>
2025-03-04 13:43:20 +0000

    feat: add company policy management and remove HR policies

--- employee_self_service/employee_self_service/doctype/company_policy/company_policy.js
+++ employee_self_service/employee_self_service/doctype/company_policy/company_policy.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Nesscale Solutions Private Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("HR Policies", {
+// frappe.ui.form.on("Company Policy", {
 // 	refresh(frm) {
 
 // 	},
