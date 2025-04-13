2df65b5de08bf2e6b389cc43df4160cbf8c78d13
nesscale-com <info@nesscale.com>
2025-04-13 11:34:03 +0000

    fix: log location added

--- employee_self_service/mobile/v1/ess.py
+++ employee_self_service/mobile/v1/ess.py
@@ -44,16 +44,19 @@ from employee_self_service.utils import add_ess_comment
 
 
 @frappe.whitelist(allow_guest=True)
-def login(usr, pwd):
+def login(usr, pwd, unique_id=None):
     try:
         login_manager = LoginManager()
         login_manager.authenticate(usr, pwd)
         validate_employee(login_manager.user)
+        emp_data = get_employee_by_user(login_manager.user, fields=["name", "gender"])
+        # Register device (throws exception if device is not valid)
+        if unique_id:
+            if not register_device(emp_data.get("name"), unique_id):
+                return
         login_manager.post_login()
         if frappe.response["message"] == "Logged In":
-            emp_data = get_employee_by_user(
-                login_manager.user, fields=["name", "gender"]
-            )
+
             frappe.response["user"] = login_manager.user
             frappe.response["key_details"] = generate_key(login_manager.user)
             frappe.response["employee_id"] = emp_data.get("name")
@@ -71,6 +74,41 @@ def validate_employee(user):
     if not frappe.db.exists("Employee", dict(user_id=user)):
         frappe.response["message"] = "Please link Employee with this user"
         raise frappe.AuthenticationError(frappe.response["message"])
+
+
+def register_device(employee, unique_id):
+    # check if device registration exists for this employee
+    # if not enter the given number and create registration
+    # if exists than validate the given number with existing number
+    # if number mataches than allow login
+    # else through frappe exceptions
+    try:
+        ess_settings = get_ess_settings()
+        if not ess_settings.get("enable_device_restrictions"):
+            return True
+
+        existing_registration = frappe.db.exists(
+            "Employee Device Registration", {"employee": employee}
+        )
+
+        if not existing_registration:
+            # Register the device if not exists
+            doc = frappe.new_doc("Employee Device Registration")
+            doc.employee = employee
+            doc.unique_id = unique_id
+            doc.insert(ignore_permissions=True)
+        else:
+            # Fetch the existing device_id to compare
+            registered_device_id = frappe.db.get_value(
+                "Employee Device Registration", existing_registration, "unique_id"
+            )
+            if registered_device_id != unique_id:
+                gen_response(500, "Device not recognized. Please contact admin.")
+                return False
+        return True
+    except Exception as e:
+        frappe.log_error(frappe.get_traceback(), "register_device_error")
+        frappe.throw("An error occurred during device registration.")
 
 
 @frappe.whitelist()
@@ -607,11 +645,11 @@ def get_attendance_details_dashboard():
 def get_last_log_details(employee):
     log_details = frappe.db.sql(
         """SELECT log_type,
-		time
-		FROM `tabEmployee Checkin`
-		WHERE employee=%s
-		AND DATE(time)=%s
-		ORDER BY time DESC""",
+        time
+        FROM `tabEmployee Checkin`
+        WHERE employee=%s
+        AND DATE(time)=%s
+        ORDER BY time DESC""",
         (employee, today()),
         as_dict=1,
     )
@@ -958,25 +996,25 @@ def get_employees_having_an_event_today(event_type, date=None):
     employees_born_today = frappe.db.multisql(
         {
             "mariadb": f"""
-			SELECT `name` as 'emp_id',`personal_email`, `company`, `company_email`, `user_id`, `employee_name` AS 'name', `image`, `date_of_joining`
-			FROM `tabEmployee`
-			WHERE
-				DAY({condition_column}) = DAY(%(today)s)
-			AND
-				MONTH({condition_column}) = MONTH(%(today)s)
-			AND
-				`status` = 'Active'
-		""",
+            SELECT `name` as 'emp_id',`personal_email`, `company`, `company_email`, `user_id`, `employee_name` AS 'name', `image`, `date_of_joining`
+            FROM `tabEmployee`
+            WHERE
+                DAY({condition_column}) = DAY(%(today)s)
+            AND
+                MONTH({condition_column}) = MONTH(%(today)s)
+            AND
+                `status` = 'Active'
+        """,
             "postgres": f"""
-			SELECT "name" AS 'emp_id',"personal_email", "company", "company_email", "user_id", "employee_name" AS 'name', "image"
-			FROM "tabEmployee"
-			WHERE
-				DATE_PART('day', {condition_column}) = date_part('day', %(today)s)
-			AND
-				DATE_PART('month', {condition_column}) = date_part('month', %(today)s)    
-			AND
-				"status" = 'Active'
-		""",
+            SELECT "name" AS 'emp_id',"personal_email", "company", "company_email", "user_id", "employee_name" AS 'name', "image"
+            FROM "tabEmployee"
+            WHERE
+                DATE_PART('day', {condition_column}) = date_part('day', %(today)s)
+            AND
+                DATE_PART('month', {condition_column}) = date_part('month', %(today)s)    
+            AND
+                "status" = 'Active'
+        """,
         },
         dict(today=getdate(date), condition_column=condition_column),
         as_dict=1,
@@ -1715,6 +1753,16 @@ def employee_device_info(**kwargs):
                 )
             ).insert(ignore_permissions=True)
 
+        emp_data = get_employee_by_user(frappe.session.user)
+        existing_registration = frappe.db.exists(
+            "Employee Device Registration", {"employee": emp_data.get("name")}
+        )
+        if not existing_registration and data.get("unique_id"):
+            # Register the device if not exists
+            doc = frappe.new_doc("Employee Device Registration")
+            doc.employee = emp_data.get("name")
+            doc.unique_id = data.get("unique_id")
+            doc.insert(ignore_permissions=True)
         return gen_response(200, "Device information saved successfully!")
     except Exception as e:
         return exception_handler(e)
