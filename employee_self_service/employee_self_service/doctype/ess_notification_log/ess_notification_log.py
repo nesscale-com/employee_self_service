f6cb52bc21d7b63de0b93c30cd9503e4d6653cea
Bhavesh Maheshwari <bhavesh@pulllogic.com>
2025-09-05 09:49:52 +0000

    fix: update notification

--- employee_self_service/employee_self_service/doctype/ess_notification_log/ess_notification_log.py
+++ employee_self_service/employee_self_service/doctype/ess_notification_log/ess_notification_log.py
@@ -8,40 +8,40 @@ import json
 
 
 class ESSNotificationLog(Document):
-	def after_insert(self):
-		target_site_url = "https://notification.nesscale.com/api/method/ncs_nesscale.api.send_push_notification"
+    def after_insert(self):
+        target_site_url = "https://notification.nesscale.com/api/method/ncs_nesscale.api.send_push_notification"
 
-		erp_url = frappe.utils.get_url()
+        erp_url = frappe.utils.get_url()
 
-		# Prepare the payload
-		payload = {
-			"product_name": "Nesscale ESS",
-			"subject": self.subject,
-			"message": self.message,
-			"notification_type": "info",
-			"tokens": [self.token],
-			"erp_url": erp_url,
-			"reference_document": self.reference_document,
-			"reference_name": self.reference_name,
-			"other_info": self.other_info,
-		}
-		# Set your headers for authentication (API key and secret)
-		headers = {"Content-Type": "application/json"}
+        # Prepare the payload
+        payload = {
+            "product_name": "Pollen ESS",
+            "subject": self.subject,
+            "message": self.message,
+            "notification_type": "info",
+            "tokens": [self.token],
+            "erp_url": erp_url,
+            "reference_document": self.reference_document,
+            "reference_name": self.reference_name,
+            "other_info": self.other_info,
+        }
+        # Set your headers for authentication (API key and secret)
+        headers = {"Content-Type": "application/json"}
 
-		try:
-			# Send the POST request
-			response = requests.post(
-				target_site_url, headers=headers, data=json.dumps(payload)
-			)
-			# Check the response
-			if response.status_code == 200:
-				print("Notification sent successfully!")
-			else:
-				frappe.log_error(
-					title="ESS Push Notification Error",
-					message=f"Failed to send notification. Status Code: {response.status_code}, Response: {response.text}",
-				)
-		except Exception as e:
-			frappe.log_error(
-				title="ESS Push Notification Error", message=frappe.get_traceback()
-			)
+        try:
+            # Send the POST request
+            response = requests.post(
+                target_site_url, headers=headers, data=json.dumps(payload)
+            )
+            # Check the response
+            if response.status_code == 200:
+                print("Notification sent successfully!")
+            else:
+                frappe.log_error(
+                    title="ESS Push Notification Error",
+                    message=f"Failed to send notification. Status Code: {response.status_code}, Response: {response.text}",
+                )
+        except Exception as e:
+            frappe.log_error(
+                title="ESS Push Notification Error", message=frappe.get_traceback()
+            )
