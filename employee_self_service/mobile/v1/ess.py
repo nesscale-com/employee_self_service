10dc6d6f7febded928b5d44800866d0c2ec5df8f
Jignesh <jignesh@nesscale.com>
2025-09-22 13:07:50 +0000

    revert: ess api changes

--- employee_self_service/mobile/v1/ess.py
+++ employee_self_service/mobile/v1/ess.py
@@ -31,6 +31,8 @@ from employee_self_service.mobile.v1.api_utils import (
     exception_handler,
     convert_timezone,
     get_system_timezone,
+    get_till_date_holiday_month_wise,
+    get_mobile_app_route,
 )
 from frappe.handler import upload_file
 from erpnext.accounts.utils import get_fiscal_year
@@ -601,7 +603,7 @@ def get_dashboard():
         dashboard_data["employee_name"] = emp_data.get("employee_name")
         get_latest_expense(dashboard_data, emp_data.get("name"))
         get_latest_ss(dashboard_data, emp_data.get("name"))
-        # get_latest_leave(dashboard_data, emp_data.get("name"))
+        get_latest_leave(dashboard_data, emp_data.get("name"))
         get_last_log_type(dashboard_data, emp_data.get("name"))
         return gen_response(200, "Dashboard data get successfully", dashboard_data)
 
@@ -2172,9 +2174,6 @@ def get_profile_detail_tabs():
         response["education_details"] = education_details
 
         bank_details = {}
-        bank_details["bank_name"] = emp_doc.get("bank_name") or ""
-        bank_details["bank_ac_no"] = emp_doc.get("bank_ac_no") or ""
-        bank_details["iban"] = emp_doc.get("iban") or ""
         bank_details["bank_name"] = emp_doc.get("bank_name") or ""
         bank_details["bank_ac_no"] = emp_doc.get("bank_ac_no") or ""
         bank_details["iban"] = emp_doc.get("iban") or ""
