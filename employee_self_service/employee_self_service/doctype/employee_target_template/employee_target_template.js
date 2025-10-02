95bf56c5de6046273c6c1a5820fbaa0e52691420
Jignesh <jignesh@nesscale.com>
2025-10-02 13:38:27 +0000

    fix:enhance item group filtering in employee target template

--- employee_self_service/employee_self_service/doctype/employee_target_template/employee_target_template.js
+++ employee_self_service/employee_self_service/doctype/employee_target_template/employee_target_template.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2025, Nesscale Solutions Private Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Target Template", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee Target Template", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+});
+
+frappe.ui.form.on("Target Item Group List", {
+    item_group: function(frm, cdt, cdn) {
+        set_item_group_query(frm);
+    }
+});
+
+function set_item_group_query(frm) {
+    frm.fields_dict['item_group_list'].grid.get_field('item_group').get_query = function(doc, cdt, cdn) {
+        let child_row = locals[cdt][cdn];
+        let selected_groups = [];
+
+        if (doc.item_group_list) {
+            doc.item_group_list.forEach(function(row) {
+                if (row.item_group && row.name !== child_row.name) {
+                    selected_groups.push(row.item_group);
+                }
+            });
+        }
+
+        return {
+            query: 'employee_self_service.employee_self_service.doctype.employee_target_template.employee_target_template.get_filtered_item_groups',
+            filters: {
+                'selected_groups': selected_groups
+            }
+        };
+    };
+
+    frm.refresh_field('item_group_list');
+}
--- employee_self_service/employee_self_service/doctype/employee_target_template/employee_target_template.py
+++ employee_self_service/employee_self_service/doctype/employee_target_template/employee_target_template.py
@@ -1,9 +1,41 @@
 # Copyright (c) 2025, Nesscale Solutions Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EmployeeTargetTemplate(Document):
-	pass
+    pass
+
+
+@frappe.whitelist()
+def get_filtered_item_groups(doctype, txt, searchfield, start, page_len, filters):
+    selected_groups = filters.get("selected_groups", [])
+
+    if not selected_groups:
+        selected_groups = []
+
+    all_item_groups = frappe.get_all("Item Group", fields=["name", "parent_item_group"])
+    exclude_set = set()
+
+    for group in selected_groups:
+        if not group:
+            continue
+
+        exclude_set.add(group)
+        for item in all_item_groups:
+            # add parent
+            if item.name == group and item.parent_item_group:
+                exclude_set.add(item.parent_item_group)
+
+            # add children
+            if item.parent_item_group == group:
+                exclude_set.add(item.name)
+
+    results = []
+    for item in all_item_groups:
+        if item.name in exclude_set:
+            continue
+        results.append([item.name, item.parent_item_group])
+    return results
