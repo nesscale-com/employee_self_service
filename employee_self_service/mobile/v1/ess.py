f6d571d5610aa4368e5e538e8f1bbf6c8e0bcdb6
nesscale-com <info@nesscale.com>
2025-11-05 10:30:04 +0000

    feat: add filter option for attendance report based on month

--- employee_self_service/mobile/v1/ess.py
+++ employee_self_service/mobile/v1/ess.py
@@ -745,6 +745,7 @@ def get_attendance_details(emp_data, year=None, month=None):
 @frappe.whitelist()
 def run_attendance_report(employee, company):
     filters = {
+        "filter_based_on": "Month",
         "month": cstr(frappe.utils.getdate().month),
         "year": cstr(frappe.utils.getdate().year),
         "company": company,
