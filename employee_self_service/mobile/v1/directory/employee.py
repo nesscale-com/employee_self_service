87997090f735fd64d8b331f0847e30e9ca803015
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2026-01-09 11:15:59 +0000

    refactor: comment out employee retrieval logic in get_employee_directory function

--- employee_self_service/mobile/v1/directory/employee.py
+++ employee_self_service/mobile/v1/directory/employee.py
@@ -15,31 +15,31 @@ from employee_self_service.mobile.v1.api_utils import (
 @ess_validate(methods=["GET"])
 def get_employee_directory(start=0, page_length=10, filters=None):
     try:
-        current_user = frappe.session.user
-        frappe.set_user("Administrator")
-        status_filter = ["status", "=", "Active"]
-        if filters:
-            filters.append(status_filter)
-        else:
-            filters = [status_filter]
-        employee = frappe.get_all(
-            "Employee",
-            filters=filters,
-            fields=[
-                "name",
-                "employee_name",
-                "cell_number",
-                "company_email",
-                "designation",
-                "department",
-                "branch",
-                "image",
-            ],
-            start=start,
-            page_length=page_length,
-        )
-        frappe.set_user(current_user)
-        return gen_response(200, "Employee list Getting Successfully", employee)
+        # current_user = frappe.session.user
+        # frappe.set_user("Administrator")
+        # status_filter = ["status", "=", "Active"]
+        # if filters:
+        #     filters.append(status_filter)
+        # else:
+        #     filters = [status_filter]
+        # employee = frappe.get_all(
+        #     "Employee",
+        #     filters=filters,
+        #     fields=[
+        #         "name",
+        #         "employee_name",
+        #         "cell_number",
+        #         "company_email",
+        #         "designation",
+        #         "department",
+        #         "branch",
+        #         "image",
+        #     ],
+        #     start=start,
+        #     page_length=page_length,
+        # )
+        # frappe.set_user(current_user)
+        return gen_response(200, "Employee list Getting Successfully", [])
     except frappe.PermissionError:
         return gen_response(500, "Not permitted read employee")
     except Exception as e:
