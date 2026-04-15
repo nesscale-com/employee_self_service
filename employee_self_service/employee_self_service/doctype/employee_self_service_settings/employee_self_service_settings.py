e63f3eae1bdbec3f97d922de46843dec7eb24807
Manav <manavmandli2990@gmail.com>
2026-04-15 17:43:34 +0530

    feat: add location validation and geolocation tracking update in EmployeeSelfServiceSettings

--- employee_self_service/employee_self_service/doctype/employee_self_service_settings/employee_self_service_settings.py
+++ employee_self_service/employee_self_service/doctype/employee_self_service_settings/employee_self_service_settings.py
@@ -1,9 +1,21 @@
 # Copyright (c) 2022, Nesscale Solutions Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class EmployeeSelfServiceSettings(Document):
-    pass
+
+    def validate(self):
+        if self.location_validate == 0:
+            self.update_hr_settings_geolocation()
+
+    def update_hr_settings_geolocation(self):
+        if frappe.db.exists("DocType", "HR Settings"):
+            frappe.db.set_value(
+                "HR Settings",
+                "HR Settings",
+                "allow_geolocation_tracking",
+                0,
+            )
