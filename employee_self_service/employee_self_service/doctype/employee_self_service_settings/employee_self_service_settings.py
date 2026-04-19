2595e47101b259a766517bf8abca2e17dc61398b
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2026-04-19 19:17:39 +0000

    fix: order new flags added, visit with attachment added

--- employee_self_service/employee_self_service/doctype/employee_self_service_settings/employee_self_service_settings.py
+++ employee_self_service/employee_self_service/doctype/employee_self_service_settings/employee_self_service_settings.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2022, Nesscale Solutions Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
+
+from employee_self_service.setup import disable_geolocation_tracking
 
 
 class EmployeeSelfServiceSettings(Document):
-    pass
+    def on_update(self):
+        disable_geolocation_tracking()
